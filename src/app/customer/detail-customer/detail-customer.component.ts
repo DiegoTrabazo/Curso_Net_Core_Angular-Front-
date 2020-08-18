be59b2e494cb330b3fa0742af9f7b645328db757
Diego Trabazo <diego.trabazo@outlook.com>
2020-08-18 12:01:03 -0300

    Add ChangeDetectionStrategy to render components OnPush

--- src/app/customer/detail-customer/detail-customer.component.ts
+++ src/app/customer/detail-customer/detail-customer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { DetailCustomerService } from './detail-customer.service';
 import { AddOrEditCustomer } from '../models/AddOrEditCustomer';
 import { FormBuilder } from '@angular/forms';
@@ -12,14 +12,17 @@ export interface DialogData {
   selector: 'app-detail-customer',
   templateUrl: './detail-customer.component.html',
   styleUrls: ['./detail-customer.component.scss'],
-  providers: [DetailCustomerService]
+  providers: [DetailCustomerService],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DetailCustomerComponent implements OnInit {
 
   customer: AddOrEditCustomer = new AddOrEditCustomer();
   
   constructor(private service: DetailCustomerService, private fb: FormBuilder,
-    public dialogRef: MatDialogRef<DetailCustomerComponent>, @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+    public dialogRef: MatDialogRef<DetailCustomerComponent>, 
+    @Inject(MAT_DIALOG_DATA) public data: DialogData, 
+    private ref: ChangeDetectorRef) {
       this.customerRetrieved(data.id);
      }
 
@@ -30,13 +33,7 @@ export class DetailCustomerComponent implements OnInit {
     this.service.getCustomerById(id)
       .subscribe(response => {
         this.customer = response;
-        // this.newCustomerForm.patchValue({
-        //   firstName: response.firstName,
-        //   lastName: response.lastName,
-        //   city: response.city,
-        //   country: response.country,
-        //   phone: response.phone
-        // });
+        this.ref.markForCheck();
       });
   }
 }
